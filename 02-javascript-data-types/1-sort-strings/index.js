949e07f3e4cb520ec61b16394550561e2aae7ee2
Aliaksei Sauko <alexeysju@gmail.com>
2023-06-26 15:51:03 +0300

    #02 added string comparer

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,4 +1,14 @@
 /**
+ * Determines whether two strings are equivalent in the 'ru' and 'en' locale with 'upper' case first.
+ * @param {string} s1 - The first string to compare.
+ * @param {string} s2 - The second string to compare.
+ * @returns {number}
+ */
+function stringCompareWithUpperCaseFirstInRuEnLocales(s1, s2) {
+  return s1.localeCompare(s2, ['ru', 'en'], { caseFirst: 'upper' });
+}
+
+/**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
