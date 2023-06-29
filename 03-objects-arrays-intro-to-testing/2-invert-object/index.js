820e1dcb0704e4e6874086f50f8a0e9b4bc688d6
Aliaksei Sauko <alexeysju@gmail.com>
2023-06-29 17:10:02 +0300

    03 invertObj; added implementation with tests

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,20 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+    if(obj === undefined){
+        return;
+    }
+    
+    const result = {};
 
+    for (const key in obj) {
+        if (!obj.hasOwnProperty(key)) {
+            continue;
+        }
+
+        const value = obj[key];
+        result[value] = key;
+    }
+
+    return result;
 }
--- 03-objects-arrays-intro-to-testing/2-invert-object/index.spec.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.spec.js
@@ -15,6 +15,50 @@ describe('objects-arrays-intro-to-testing/invert-object', () => {
     expect(invertObj(obj)).toEqual(expected);
   });
 
+  it('should swap keys and values and return new object. Process number values', () => {
+    const obj = {
+      numericKey1: 1,
+      numericKey2: 2,
+    };
+
+    const expected = {
+      1: 'numericKey1',
+      2: 'numericKey2',
+    };
+
+    expect(invertObj(obj)).toEqual(expected);
+  });
+
+  it('should swap keys and values and return new object. Process string with spaces', () => {
+    const obj = {
+      keyWithSpaces1: 'some text with spaces 1',
+      keyWithSpaces2: 'some text with spaces 2',
+    };
+
+    const expected = {
+      'some text with spaces 1': 'keyWithSpaces1',
+      'some text with spaces 2': 'keyWithSpaces2',
+    };
+
+    expect(invertObj(obj)).toEqual(expected);
+  });
+
+  it('should swap keys and values and return new object. Process reserved words such as "let", "var", "return"', () => {
+    const obj = {
+      letKey: 'let',
+      varKey: 'var',
+      returnKey: 'return',
+    };    
+   
+    const expected = {
+      let: 'letKey',
+      var: 'varKey',
+      return: 'returnKey',
+    };
+
+    expect(invertObj(obj)).toEqual(expected);
+  });
+
   it('should return empty object if was passed object without values', () => {
     expect(invertObj({})).toEqual({});
   });
