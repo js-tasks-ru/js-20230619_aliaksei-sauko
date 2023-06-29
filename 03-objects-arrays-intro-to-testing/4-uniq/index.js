babd8a72f8cd69341f3d4c2d7f473fb7d8c17c5e
Aliaksei Sauko <alexeysju@gmail.com>
2023-06-29 19:27:27 +0300

    03 uniq; added implementation

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,6 +3,18 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
+export function uniq(arr = []) {
+  let trimmedArray = [];
+  let currentItem;
+         
+  for (let i = 0; i < arr.length; i++) {
+    if (currentItem === arr[i]) { 
+        continue;
+    }
 
+    currentItem = arr[i];
+    trimmedArray.push(currentItem);
+  }
+  
+  return trimmedArray;
 }
