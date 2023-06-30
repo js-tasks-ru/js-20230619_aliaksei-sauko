7c5c50586b96c25ec8d8e0d3823f871740baa5de
Aliaksei Sauko <alexeysju@gmail.com>
2023-07-01 02:27:34 +0300

    03 uniq; added tests

--- 03-objects-arrays-intro-to-testing/4-uniq/index.spec.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.spec.js
@@ -4,6 +4,7 @@ describe('objects-arrays-intro-to-testing/uniq', () => {
   it('should return array with uniq values', () => {
     expect(uniq([1, 1])).toEqual([1]);
     expect(uniq([1, 'a', 'a', 2, 2])).toEqual([1, 'a', 2]);
+    expect(uniq([1, 2, 1, 1])).toEqual([1, 2, 1]);
   });
 
   it('should return empty array if was passed array without values', () => {
