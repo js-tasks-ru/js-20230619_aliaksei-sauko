0adffcdc9d8bbc4578e3f614f72e109d43032033
Aliaksei Sauko <alexeysju@gmail.com>
2023-07-11 07:58:44 +0300

    06 1-sortable-table-v2; added a sort arrow element generator to the header cell template

--- 05-dom-document-loading/2-sortable-table-v1/templates/header-cell-template.js
+++ 05-dom-document-loading/2-sortable-table-v1/templates/header-cell-template.js
@@ -27,19 +27,35 @@ export class HeaderCellTemplate {
   //
   // static
 
+  static generateSortArrowElement() {
+    const template = document.createElement('div');
+    template.innerHTML = this.getSortArrowTemplate();
+
+    return template.firstElementChild;
+  }
+
   static fillTemplate(id, title, sortable, order = '') {
     const template = HeaderCellTemplate.getTemplate();
+    const sortArrowTemplate = order ? this.getSortArrowTemplate() : '';
 
     return template
       .replace(/__ID__/g, id)
       .replace(/__TITLE__/g, title)
       .replace(/__ORDER__/g, order)
+      .replace(/__SORTARROW__/g, sortArrowTemplate)
       .replace(/__SORTABLE__/g, sortable);
   }
 
   static getTemplate() {
     return `<div class="sortable-table__cell" data-id="__ID__" data-sortable="__SORTABLE__" data-order="__ORDER__">
                 <span>__TITLE__</span>
+                __SORTARROW__
             </div>`;
+  }
+
+  static getSortArrowTemplate() {
+    return `<span data-element="arrow" class="sortable-table__sort-arrow">
+                <span class="sort-arrow"></span>
+            </span>`;
   }
 }
